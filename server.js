10781e063841095bd906ef6e31c4a446dfe2fee7
muskan-dangol <dangolmuskan19@gmail.com>
2023-09-03 11:46:22 +0300

    Add post for TODO

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
+const bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var TODO = [{
-    id: 1, 
-    description: 'Meet mum for lunch',
-    completed: false
-}, {
-    id:2, 
-    description: 'go shopping',
-    completed: false
-}, {
-    id:3, 
-    description: 'do laundry in the evening!',
-    completed: true
-}]
+var TODO = []
+var TODOnextId = 1;
 
+app.use(bodyParser.json()); //lets set up middleware
 
 app.get('/', function(req, res) {
     res.send('Root Api to TODO app');
@@ -42,6 +33,13 @@ app.get('/TODO/:id', function(req, res) {
     res.send('Requesting todo with id of ' + req.params.id)
 });
 
+
+app.post('/TODO', function(req, res){
+    var body = req.body;
+    body.id = TODOnextId++;
+    TODO.push(body);
+    res.json(body);
+});
 
 app.listen(PORT, function() {
     console.log('Express is listening to the port ' + PORT + '!')
